1663ced6ec38efe76b87afb741506d00bc0edfb1
Ilya Sakharov <cj.ilya@yandex.ru>
2025-12-02 18:04:53 +0300

    webinar 14-16 csv-logs

--- date_format.py
+++ date_format.py
@@ -1,14 +1,24 @@
-from datetime import datetime
+import csv
 
-moscow_times_str = "Wednesday, October 2, 2002"
-guardian_str = "Friday, 11.10.13"
-daily_news_str = "Thursday, 18 August 1977"
+purchase_dict = {}
 
-moscow_times_date = datetime.strptime(moscow_times_str, "%A, %B %d, %Y")
-guardian_date = datetime.strptime(guardian_str, "%A, %d.%m.%y")
-daily_news_date = datetime.strptime(daily_news_str, "%A, %d %B %Y")
+with open('purchase_log.csv', 'r', encoding='utf-8') as f_purchases:
+    reader = csv.DictReader(f_purchases)
+    for row in reader:
+        user_id = row['user_id']
+        category = row['category']
+        purchase_dict[user_id] = category
 
-# Вывод результатов
-print("The Moscow Times:", moscow_times_date)
-print("The Guardian:", guardian_date)
-print("Daily News:", daily_news_date)
+with open('visit_log.csv', 'r', encoding='utf-8') as f_visits, \
+     open('funnel.csv', 'w', encoding='utf-8', newline='') as f_funnel:
+
+    visits_reader = csv.DictReader(f_visits)
+    fieldnames = visits_reader.fieldnames + ['category']
+
+    funnel_writer = csv.DictWriter(f_funnel, fieldnames=fieldnames)
+    funnel_writer.writeheader()
+    for row in visits_reader:
+        user_id = row['user_id']
+        if user_id in purchase_dict:
+            row['category'] = purchase_dict[user_id]
+            funnel_writer.writerow(row)
